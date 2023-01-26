297b0baf6a5c4f3477e56011e687f61c0e5cf078
whffu762 <whffu762@naver.com>
2023-01-26 21:55:13 +0900

    git-a-test1

---
+++ App2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Seat {
+    private String name;
+
+    public Seat() {
+        this.name = "_ _ _";
+    }
+
+    public void SetName(String name) {
+        this.name = name;
+    }
+
+    public String GetName() {
+        return this.name;
+    }
+
+}
+
+
+public class App2{
+    
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("입력 : ");
+        String x =scan.nextLine();
+
+        System.out.print("입력 1 : ");
+        int y = scan.nextInt();
+
+        System.out.println(x + " " + y);
+
+        Seat yy = new Seat();
+        yy.SetName(x);
+
+        System.out.println(yy.GetName());
+        scan.close();
+    }
+}
